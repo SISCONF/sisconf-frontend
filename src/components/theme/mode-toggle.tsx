eb2bfd6b18455e1c31e16959932f0976e6891016
Henrique Eduardo <heduardocr7@gmail.com>
2024-12-05 20:32:51 -0300

    feat: add component to change theme

---
+++ src/components/theme/mode-toggle.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      variant="outline"
+      size="icon"
+      aria-label={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+    >
+      {theme === "light" ? (
+        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  );
+}
