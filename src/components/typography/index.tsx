81c0384a191ae7ee80f2753024e9d33c31866aa4
Henrique Eduardo <heduardocr7@gmail.com>
2025-02-04 18:54:40 -0300

    add dynamic support for variant-based HTML tags

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -1,29 +1,48 @@
 import { cn } from "@/lib/utils";
-import { VariantProps } from "class-variance-authority";
-import { ComponentProps, ReactNode } from "react";
+import type { VariantProps } from "class-variance-authority";
+import type { ElementType, ReactNode } from "react";
 import { typographyVariants } from "./typography-variants";
 
-export interface TypographyGroupProps
-  extends VariantProps<typeof typographyVariants>,
-    ComponentProps<"span"> {
+export interface TypographyProps
+  extends VariantProps<typeof typographyVariants> {
   children: ReactNode;
   className?: string;
+  as?: ElementType;
 }
+
+const tags = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  body1: "p",
+  body2: "p",
+} as const;
 
 export function Typography({
   children,
   className,
-  variant,
+  variant = "h4",
   fontWeight,
+  colorText,
+  as,
   ...props
-}: TypographyGroupProps) {
+}: TypographyProps) {
+  const validVariant = variant ?? "h4";
+  const Tag: ElementType =
+    as || tags[validVariant as keyof typeof tags] || "span";
+
   return (
-    <span
-      data-testid="typography-container"
-      className={cn(typographyVariants({ variant, fontWeight }), className)}
+    <Tag
+      className={cn(
+        typographyVariants({ variant, fontWeight, colorText }),
+        className
+      )}
       {...props}
     >
       {children}
-    </span>
+    </Tag>
   );
 }
