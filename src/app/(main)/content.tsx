84c6ba6d577184cb8ba33d0fee221dea2d97cdca
Henrique Eduardo <heduardocr7@gmail.com>
2025-02-08 11:45:58 -0300

    feat: render component on home page

---
+++ src/app/(main)/content.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { ComponentTabs } from "@/components/tabs";
+import { ModeToggle } from "@/components/theme/mode-toggle";
+import { useState } from "react";
+
+export function Content() {
+  const TABS_LIST = [
+    {
+      id: 0,
+      name: "frutas",
+      label: "Frutas",
+      component: <div>Ola</div>,
+    },
+    {
+      id: 1,
+      name: "verduras",
+      label: "Verduras",
+      component: <div>Tudo</div>,
+    },
+    {
+      id: 2,
+      name: "legumes",
+      label: "Legumes",
+      component: <div>Bem?</div>,
+    },
+
+    {
+      id: 3,
+      name: "outros",
+      label: "Outros",
+      component: <div>Bem?</div>,
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState(TABS_LIST[0]);
+
+  const handleTabChange = (id: number) => {
+    const selectedTab = TABS_LIST.find((tab) => tab.id === id);
+    setActiveTab(selectedTab || TABS_LIST[0]);
+  };
+
+  return (
+    <main className="flex flex-col justify-center items-center w-full h-screen bg-background gap-2">
+      <ModeToggle />
+      <ComponentTabs
+        tabList={TABS_LIST}
+        defaultValue="frutas"
+        onTabChange={handleTabChange}
+      />
+
+      {activeTab.component}
+    </main>
+  );
+}
