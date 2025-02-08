b061e128f5a32abbeeeecfd530d8c038b1e6d6d2
Henrique <azevedo.henrique@escolar.ifrn.edu.br>
2025-02-08 10:05:31 -0300

    Adicionar Dark Mode

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/theme/mode-toggle";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col min-h-screen bg-background">
-      <main className="w-full h-full"></main>
-    </div>
+    <main className="flex justify-center items-center w-full h-screen bg-background">
+      <div className="">
+        <ModeToggle />
+      </div>
+    </main>
   );
 }
