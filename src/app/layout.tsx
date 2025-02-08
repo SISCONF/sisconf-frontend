38f2956eec54af58e84cae676b9b9adf2972e0e6
Henrique Eduardo <heduardocr7@gmail.com>
2025-02-08 11:20:41 -0300

    feat: add React Query for state management and data caching and client method to connect to API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Registry } from "./registry";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,18 +17,7 @@ export default function RootLayout({
       <head>
         <meta httpEquiv="Access-Control-Allow-Origin" content="*" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <Registry>{children}</Registry>
     </html>
   );
 }
