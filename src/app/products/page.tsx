d42d53da03ecee01fed967193b082ef0b6b9aff7
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-02-15 18:11:06 -0300

    fix: melhora responsividade para telas maiores e corrige alguns erros de estilo em telas médias/largas.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,8 @@ import ProductsSession from "@/components/productsSession";
 
 // Images
 import VeggiesHeader from "/public/assets/products-header-veggies.png";
-import StrawberriesBanenr from "/public/assets/strawberries-banner.png";
-import CarrotsBanner from "/public/assets/carrots-banner.png";
+import StrawberriesBanenr from "/public/assets/strawberries-banner.jpg";
+import CarrotsBanner from "/public/assets/carrots-banner.jpg";
 
 export default function Products() {
   return (
@@ -24,13 +24,18 @@ export default function Products() {
           Explore nosso <span className="text-[#43A046]">catálogo</span>
         </Typography>
       </header>
-      <main className="border-b-gray-950 border py-[1.56rem] px-[1.875rem] flex gap-[2.5rem] items-center">
+      <main className="h-screen border py-[1.56rem] px-[1.875rem] flex gap-[2.5rem]">
         <Image
+          className="h-full rounded-[1.25rem] object-cover max-w-[280px] 2xl:max-w-[500px]"
           src={StrawberriesBanenr}
           alt="a bunch of strawberries together"
         />
         <ProductsSession />
-        <Image src={CarrotsBanner} alt="a bunch of carrots together" />
+        <Image
+          className="h-full rounded-[1.25rem] object-cover max-w-[280px] 2xl:max-w-[500px]"
+          src={CarrotsBanner}
+          alt="a bunch of carrots together"
+        />
       </main>
     </>
   );
--- src/components/productsSession/index.tsx
+++ src/components/productsSession/index.tsx
@@ -4,7 +4,7 @@ import ProductCard from "../productCard";
 
 export default function ProductsSession() {
   return (
-    <div className="w-full">
+    <div className="border border-black w-full">
       <div className="border border-black">The tabs stay here</div>
       <section>
         <SearchProductAndContact />
