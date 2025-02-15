dccbab978109217a35e455bef6c6533b437bca59
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-02-15 16:00:34 -0300

    :sparkles: feat: adiciona a barra de pesquisa e os contêineres dos produtos.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,12 @@
+// Components
 import Image from "next/image";
-import VeggiesHeader from "/public/assets/products-header-veggies.png";
 import { Typography } from "@/components/typography";
+import ProductsSession from "@/components/productsSession";
+
+// Images
+import VeggiesHeader from "/public/assets/products-header-veggies.png";
+import StrawberriesBanenr from "/public/assets/strawberries-banner.png";
+import CarrotsBanner from "/public/assets/carrots-banner.png";
 
 export default function Products() {
   return (
@@ -18,7 +24,14 @@ export default function Products() {
           Explore nosso <span className="text-[#43A046]">catálogo</span>
         </Typography>
       </header>
-      <main className="border-b-gray-950 border">Main</main>
+      <main className="border-b-gray-950 border py-[1.56rem] px-[1.875rem] flex justify-between items-center">
+        <Image
+          src={StrawberriesBanenr}
+          alt="a bunch of strawberries together"
+        />
+        <ProductsSession />
+        <Image src={CarrotsBanner} alt="a bunch of carrots together" />
+      </main>
     </>
   );
 }
