a1899c6905b1030e545f190b02f0d670c8521888
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-02-23 20:17:57 -0300

    cleanup: remove tabela pura para poder utilizar data-tables.

--- src/app/(protected)/dashboard/_components/stock-content.tsx
+++ src/app/(protected)/dashboard/_components/stock-content.tsx
@@ -1,54 +1,47 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import Quantity from "@/components/quantity";
-import Image from "next/image";
-
 import FoodPlaceholderImage from "/public/assets/food-placeholder.jpg";
+
+const dummyData = [
+  {
+    id: 1,
+    image: FoodPlaceholderImage,
+    food: "Maçã",
+    type: "Fruta",
+    amount: 2,
+  },
+  {
+    id: 2,
+    image: FoodPlaceholderImage,
+    food: "Maçã",
+    type: "Fruta",
+    amount: 5,
+  },
+  {
+    id: 3,
+    image: FoodPlaceholderImage,
+    food: "Maçã",
+    type: "Fruta",
+    amount: 3,
+  },
+  {
+    id: 4,
+    image: FoodPlaceholderImage,
+    food: "Maçã",
+    type: "Fruta",
+    amount: 7,
+  },
+  {
+    id: 5,
+    image: FoodPlaceholderImage,
+    food: "Maçã",
+    type: "Fruta",
+    amount: 11,
+  },
+];
 
 export default function StockContent() {
   return (
     <div className="border border-black px-[3rem] py-[3rem]">
-      <Table className="text-[0.875rem]">
-        <TableCaption>Comidas que estão no seu estoque.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>#</TableHead>
-            <TableHead>Comida</TableHead>
-            <TableHead>Tipo</TableHead>
-            <TableHead>Quantidade</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => (
-            <TableRow>
-              <TableCell>{id}</TableCell>
-              <TableCell>
-                <div className="flex gap-[1rem] items-center">
-                  <div className="w-[6.875rem] rounded-lg inline-block">
-                    <Image
-                      src={FoodPlaceholderImage}
-                      alt="a red apple in a gray background."
-                      className="rounded-lg"
-                    />
-                  </div>
-                  <span>Maçã</span>
-                </div>
-              </TableCell>
-              <TableCell>Fruta</TableCell>
-              <TableCell>
-                <Quantity />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      Hello World :)
     </div>
   );
 }
