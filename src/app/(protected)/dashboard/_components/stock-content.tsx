535f387603a031cbed8821e9d95a930d303da339
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-02-23 21:16:34 -0300

    feat: adiciona (estaticamente) os botões de atualizar quantidade de comidas e de removê-las do estoque.

--- src/app/(protected)/dashboard/_components/stock-content.tsx
+++ src/app/(protected)/dashboard/_components/stock-content.tsx
@@ -2,31 +2,41 @@ import FoodPlaceholderImage from "/public/assets/food-placeholder.jpg";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "./columns";
 import { StockFood } from "./columns";
+import { Button } from "@/components/ui/button";
+import { Trash2Icon } from "lucide-react";
 
 const dummyData: StockFood[] = [
   {
     id: 1,
     food: { image: FoodPlaceholderImage, title: "Maçã" },
     type: "Fruta",
-    amount: 2,
+    amount: 1,
   },
   {
     id: 2,
     food: { image: FoodPlaceholderImage, title: "Banana" },
     type: "Fruta",
-    amount: 10,
+    amount: 1,
   },
   {
     id: 3,
     food: { image: FoodPlaceholderImage, title: "Uva" },
     type: "Fruta",
-    amount: 6,
+    amount: 1,
   },
 ];
 
 export default function StockContent() {
   return (
-    <div className="border border-black px-[3rem] py-[3rem]">
+    <div className="px-[3rem] py-[3rem]">
+      <div className="flex gap-4 pb-4">
+        <Button className="bg-green-700 hover:bg-green-900">
+          Atualizar quantidade
+        </Button>
+        <Button className="bg-red-500 hover:bg-red-700">
+          <Trash2Icon />
+        </Button>
+      </div>
       <DataTable data={dummyData} columns={columns} />
     </div>
   );
