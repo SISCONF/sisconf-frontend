9016ccea6ee5fb053fd2115478dd8ef6fa880d23
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-03-01 20:51:14 -0300

    fix: adds '/api' string to default API_HOST value and fixes import for PageTitle and OrdersList components.

--- src/const/envs.ts
+++ src/const/envs.ts
@@ -1,5 +1,5 @@
 export const API_HOST =
-  process.env.API_HOST?.replace(/"/g, "") || "http://localhost:8080";
+  process.env.API_HOST?.replace(/"/g, "") || "http://localhost:8080/api";
 export const IMAGES_HOST = process.env.NEXT_PUBLIC_IMAGES_HOST?.replace(
   /"/g,
   ""
