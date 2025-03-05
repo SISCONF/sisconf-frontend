5f852bb578601fd2436b3ef572a05481695fb589
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-03-05 16:12:47 -0300

    fix: each dashboard tab now properly displays the content it should instead of displaying all tabs' contents.

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -90,12 +90,10 @@ export default function Page() {
     useState<OrdersGroup | null>(null);
   const [selectedNavItem, setSelectedNavItem] = useState("Pedidos");
 
-  const { 
-    data
-  } = useQuery({
+  const { data } = useQuery({
     queryKey: ["orders"],
-    queryFn: () => fetchOrders()
-  })
+    queryFn: () => fetchOrders(),
+  });
 
   const handleNavigation = (item: string) => {
     setSelectedNavItem(item);
@@ -124,32 +122,20 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div> */}
-        {/* <StockContent /> */}
-
-        {selectedNavItem === "Estoque" && <StockContent />}
-        {selectedNavItem === "Pedidos" && 
-            selectedOrdersGroup ? (
-              <OrdersGroupDetails 
-                selectedOrdersGroup={selectedOrdersGroup}
-                setSelectedOrdersGroup={setSelectedOrdersGroup}
-              />
-            ) : (
-              <EntrepreneurOrders 
-                orders={data ?? []} 
-                ordersGroup={ordersGroup}
-                setSelectedOrdersGroup={setSelectedOrdersGroup}
-              />
-            )
-        }
-
+        {selectedNavItem === "Estoque" ? (
+          <StockContent />
+        ) : selectedOrdersGroup ? (
+          <OrdersGroupDetails
+            selectedOrdersGroup={selectedOrdersGroup}
+            setSelectedOrdersGroup={setSelectedOrdersGroup}
+          />
+        ) : (
+          <EntrepreneurOrders
+            orders={data ?? []}
+            ordersGroup={ordersGroup}
+            setSelectedOrdersGroup={setSelectedOrdersGroup}
+          />
+        )}
       </SidebarInset>
     </SidebarProvider>
   );
